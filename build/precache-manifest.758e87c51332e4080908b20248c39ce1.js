9d21ad81c13677338fedca925379b1557ede8b20
Ajay 007 <ajaybendre@credenceanalytics.com>
2020-09-25 04:27:06 +0530

    app build

---
+++ build/precache-manifest.758e87c51332e4080908b20248c39ce1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc2610b2584c40292abe3c13def4f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa3fd64ea4dc066d0d7",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "48b864290f966457724f",
+    "url": "/static/css/main.b812d917.chunk.css"
+  },
+  {
+    "revision": "2aa3fd64ea4dc066d0d7",
+    "url": "/static/js/2.dd0e081d.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.dd0e081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b864290f966457724f",
+    "url": "/static/js/main.1ebd0173.chunk.js"
+  },
+  {
+    "revision": "85290ade347f1cb7df88",
+    "url": "/static/js/runtime-main.902fdcb9.js"
+  },
+  {
+    "revision": "03f38260bb1525400e3751671ce8e10a",
+    "url": "/static/media/homevideo.03f38260.mp4"
+  },
+  {
+    "revision": "df0e69b777e7b23e6afd16c16b2de9ec",
+    "url": "/static/media/momslogo.df0e69b7.jpg"
+  }
+]);
---
+++ build/precache-manifest.e4483a5c496a2b1ff14489baf2a1495f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afa057d946ca058f90883d4bec0f326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63707fd663cdf8fdc380",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "3cdb12d59758a77b4249",
+    "url": "/static/css/main.b812d917.chunk.css"
+  },
+  {
+    "revision": "63707fd663cdf8fdc380",
+    "url": "/static/js/2.535efd81.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.535efd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdb12d59758a77b4249",
+    "url": "/static/js/main.feedae1d.chunk.js"
+  },
+  {
+    "revision": "85290ade347f1cb7df88",
+    "url": "/static/js/runtime-main.902fdcb9.js"
+  },
+  {
+    "revision": "03f38260bb1525400e3751671ce8e10a",
+    "url": "/static/media/homevideo.03f38260.mp4"
+  },
+  {
+    "revision": "df0e69b777e7b23e6afd16c16b2de9ec",
+    "url": "/static/media/momslogo.df0e69b7.jpg"
+  }
+]);
