beb6bb8fcef3e5f74afa396d39857e2023a04f5a
A00079 <bajay0007@gmail.com>
2020-09-27 02:30:08 +0530

    Bug fix in payment

---
+++ build/precache-manifest.faae0a9a8bf932af5a4e5ffea194388c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3d5225d789a69b7ee2119d8510676d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3090ecffb3bfb680b6aa",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "0fd80013e637296dda6d",
+    "url": "/static/css/main.88dad1ae.chunk.css"
+  },
+  {
+    "revision": "3090ecffb3bfb680b6aa",
+    "url": "/static/js/2.a0b60b98.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.a0b60b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd80013e637296dda6d",
+    "url": "/static/js/main.14eea292.chunk.js"
+  },
+  {
+    "revision": "85290ade347f1cb7df88",
+    "url": "/static/js/runtime-main.902fdcb9.js"
+  },
+  {
+    "revision": "03f38260bb1525400e3751671ce8e10a",
+    "url": "/static/media/homevideo.03f38260.mp4"
+  },
+  {
+    "revision": "df0e69b777e7b23e6afd16c16b2de9ec",
+    "url": "/static/media/momslogo.df0e69b7.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b6425a71b8cde3ed81e94070d043b6e.js"
+  "/precache-manifest.faae0a9a8bf932af5a4e5ffea194388c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
